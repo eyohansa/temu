a800a7aa03c3267fa694182fd55477f450f08f37
Eric Yohansa <lin.hanxiang@live.com>
2015-11-03 12:21:27 +0700

    Add sign in form.

---
+++ social/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from .models import User
+
+class SigninForm(forms.ModelForm):
+    error_messages = [
+        {'password_mismatch': "The two password fields didn't match."}
+    ]
+
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Password confirmation",
+                                       widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("username", "display_name")
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get('password')
+        password_confirm = self.cleaned_data.get('password_confirm')
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch'
+            )
+        return password_confirm
+
+    def save(self, commit=True):
+        user = super(SigninForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
