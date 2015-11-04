946b6398b26dc7c66695b08f85bbf1850c4cca54
Eric Yohansa <lin.hanxiang@live.com>
2015-11-04 10:27:49 +0700

    Add Post model.

--- social/models.py
+++ social/models.py
@@ -1,14 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=20)
-    display_name = models.TextField()
+    display_name = models.CharField(max_length=30)
     password = models.TextField()
-    join_date = models.DateTimeField('date joined')
+    join_date = models.DateField('date joined')
 
     def __str__(self):
         return self.display_name
 
     def set_password(self, new_password):
         self.password = new_password
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User)
+    post_text = models.CharField(max_length=500)
+    post_time = models.DateTimeField('time posted')
+
+    def __str__(self):
+        return self.post_text
