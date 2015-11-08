14e5ec53cc0e19bb638b41101c482868654d9b75
Eric Yohansa <lin.hanxiang@live.com>
2015-11-08 07:42:03 +0700

    Assign TemuUserManager to manage TemuUser.

--- social/models.py
+++ social/models.py
@@ -4,26 +4,6 @@ import datetime
 
 
 # Create your models here.
-class TemuUser(AbstractBaseUser):
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-
-    username = models.CharField(max_length=20, unique=True)
-    display_name = models.CharField(max_length=30)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    join_date = models.DateField('date joined')
-
-    def get_full_name(self):
-        return self.display_name
-
-    def get_short_name(self):
-        return self.display_name
-
-    def __str__(self):
-        return self.display_name
-
-
 class TemuUserManager(BaseUserManager):
     def create_user(self, username, password):
         new_user = self.model(
@@ -44,6 +24,28 @@ class TemuUserManager(BaseUserManager):
         new_superuser.set_password(password)
         new_superuser.save(using=self._db)
         return new_superuser
+
+
+class TemuUser(AbstractBaseUser):
+    username = models.CharField(max_length=20, unique=True)
+    display_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    join_date = models.DateField('date joined')
+
+    objects = TemuUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return self.display_name
+
+    def get_short_name(self):
+        return self.display_name
+
+    def __str__(self):
+        return self.display_name
 
 
 class Post(models.Model):
