4afc90426ff6be59c52fb2f38f45eb7e81b57cea
Eric Yohansa <lin.hanxiang@live.com>
2015-11-08 07:23:29 +0700

    Implement TemuUserAdmin.

--- social/admin.py
+++ social/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
+
+from .forms import SignupForm, UserUpdateForm
+from .models import TemuUser
+
 
 # Register your models here.
+class TemuUserAdmin(UserAdmin):
+    form = UserUpdateForm
+    add_form = SignupForm
+
+    list_display = ('username', 'is_superuser')
+    list_filter = ('is_superuser',)
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_superuser',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('username', 'password', 'password_confirm')
+        })
+    )
+    search_fields = ('username',)
+    ordering = ('username', 'join_date')
+    filter_horizontal = ()
+
+
+admin.site.register(TemuUser, TemuUserAdmin)
+admin.site.unregister(Group)
