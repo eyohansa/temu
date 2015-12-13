b1062354a26056f5805c3deaf5f11214b2998795
Eric Yohansa <lin.hanxiang@live.com>
2015-12-13 07:06:42 +0700

    Change general design.

--- social/static/user-page.js
+++ social/static/user-page.js
@@ -2,8 +2,18 @@ $(document).ready(function() {
     $('.commends').click(function() {
         var postId = $(this).attr("data-postid");
         $.get('/commend/', {post_id: postId}, function(data) {
-            $('#' + postId +'-commendations').html(data);
+            $('span#' + postId +'-commendation-count').html(data);
             $('#' + postId +'-commends').hide();
         });
-    })
+    });
+
+    $('.comment-submit').click(function() {
+        var postId = $(this).attr("data-postid");
+        var commentText = $('#' + postId + '-comment-input').val();
+        var url = location.pathname + postId + "/comment/";
+        console.log("Attempt to get " + url);
+        $.get(url, {post_id: postId, comment_text: commentText}, function(data) {
+           $('#' + postId + '-comments').html(data);
+        });
+    });
 });
