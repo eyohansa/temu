dcda0bb217cbf561ee3fef35a779e0646a73b6f3
Eric Yohansa <lin.hanxiang@live.com>
2015-12-18 12:19:16 +0700

    People page shows all registered users.

--- social/urls.py
+++ social/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     url(r'^signup/$', views.SignupView.as_view(), name='signup'),
     url(r'^signin/$', views.SigninView.as_view(), name='signin'),
     url(r'^signout/$', views.SignoutView.as_view(), name='signout'),
-    url(r'^commend/$', views.commend, name="commend_post"),
-    url(r'^comment/$', views.comment, name="comment_post"),
+    url(r'^commend/$', views.commend, name="commend-post"),
+    url(r'^comment/$', views.comment, name="comment-post"),
     url(r'^friend/add/$', views.add_friend, name="add_friend"),
-    url(r'^(?P<username>\w+)/$', views.UserView.as_view()),
+    url(r'^(?P<username>\w+)/$', views.UserView.as_view(), name="user-page"),
     url(r'^(?P<username>\w+)/people/$', views.PeopleView.as_view(), name='people'),
     url(r'^(?P<username>\w+)/(?P<post_id>[0-9]+)/$', views.PostView.as_view(), name="post-detail"),
 ]
